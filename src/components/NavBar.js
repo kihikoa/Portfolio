b59236e71605c8baa34b1c68a1174f278a984e9b
Angela <kihikoangela@gmail.com>
2024-10-10 14:02:18 -0400

    update

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -7,23 +7,22 @@ export const NavBar = () => {
           <Navbar.Brand href="#home">
             <img src = {' '} alt ="Logo" />
           </Navbar.Brand>
-          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Toggle aria-controls="basic-navbar-nav">
+            <span className="navbar-toogler-icon"></span>
+          </Navbar.Toggle>
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="me-auto">
               <Nav.Link href="#home">Home</Nav.Link>
-              <Nav.Link href="#link">Link</Nav.Link>
-              <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-                <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.2">
-                  Another action
-                </NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-                <NavDropdown.Divider />
-                <NavDropdown.Item href="#action/3.4">
-                  Separated link
-                </NavDropdown.Item>
-              </NavDropdown>
+              <Nav.Link href="#skills">Skills</Nav.Link>
+              <Nav.Link href="#projects">Projects</Nav.Link>
             </Nav>
+            <span className="navbar-text">
+              <div className="social-icon">
+                <a href = "#"></a>
+
+
+              </div>
+            </span>
           </Navbar.Collapse>
         </Container>
       </Navbar>    
