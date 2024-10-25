65f4136b48f01907a2c9d396f328c170b7b704ba
kihikoa <kihikoangela@gmail.com.>
2024-10-25 19:55:34 -0400

    Adding Banner

---
+++ src/components/Banner.js
@@ -0,0 +1,30 @@
+import { useState, useEffect } from "react";
+import { Container, Row, Col } from "react-bootstrap";
+import { ArrowRightCircle } from "react-bootstrap-icons";
+import headerImg from "../assets/img/header-img.svg";
+
+export const Banner = () => {
+  return (
+    <section className="banner" id="home">
+      <Container>
+        <Row className="align-items-center">
+          <Col xs={12} md={6} xl={7}>
+            <span className="tagline">Welcome to my Portfolio</span>
+            <h1>
+              {"Hi, I’m Angela"} <span className="wrap">web developer</span>
+            </h1>
+            <p>
+              Write a lil about me here nfjkenfjkensdfkldsnklfsdnmfldksnflksd
+            </p>
+            <button onClick={() => console.log("connect")}>
+              Let's Connect <ArrowRightCircle size={25} />
+            </button>
+          </Col>
+          <Col xs={12} md={6} xl={5}>
+            <img src={headerImg} alt="Header Img" />
+          </Col>
+        </Row>
+      </Container>
+    </section>
+  );
+};
